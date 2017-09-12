8473846b4e1d276294e9aa43a5d31887ea795b2b
Marcin <marcinster@gmail.com>
2017-09-12 20:56:24 +0200

    Bank initial commit

---
+++ Bank.sql
@@ -0,0 +1,11 @@
+USE BANK_v2;
+#SELECT * FROM pracownicy;
+#SELECT * FROM stanowiska;
+#SELECT * FROM dzialy;
+#imiona, nazwiska i pensje pracownikow
+#SELECT imie, nazwisko, pensja FROM pracownicy;
+
+#imiona, nazwiska i calkowite wynagrodzenie
+#SELECT imie, nazwisko, pensja+dodatki FROM pracownicy;
+
+
